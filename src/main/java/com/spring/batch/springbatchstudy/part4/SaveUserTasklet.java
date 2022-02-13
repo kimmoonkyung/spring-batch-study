e0811d7191a7bd578eb9906e4ff9fd9c22d88d47
mxxnkyung@naver.com <mxxnkyung@naver.com>
2022-02-13 23:17:00 +0900

    22.02.13 sp batch part 5 fin

--- src/main/java/com/spring/batch/springbatchstudy/part4/SaveUserTasklet.java
+++ src/main/java/com/spring/batch/springbatchstudy/part4/SaveUserTasklet.java
@@ -1,10 +1,12 @@
 package com.spring.batch.springbatchstudy.part4;
 
+import com.spring.batch.springbatchstudy.part5.Orders;
 import org.springframework.batch.core.StepContribution;
 import org.springframework.batch.core.scope.context.ChunkContext;
 import org.springframework.batch.core.step.tasklet.Tasklet;
 import org.springframework.batch.repeat.RepeatStatus;
 
+import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
@@ -12,6 +14,7 @@ import java.util.List;
 //@RequiredArgsConstructor
 public class SaveUserTasklet implements Tasklet { // user를 저장하기 위한 tasklet
 
+    private final int SIZE = 100;
     private final UserRepository userRepository;
 
     public SaveUserTasklet(UserRepository userRepository) {
@@ -32,27 +35,43 @@ public class SaveUserTasklet implements Tasklet { // user를 저장하기 위한
     private List<User> createUsers() {
         List<User> users = new ArrayList<>();
 
-        for (int i = 0; i < 100; i++) {
+        for (int i = 0; i < SIZE; i++) {
             users.add(User.builder()
-                    .totalAmount(1_000)
+                    .orders(Collections.singletonList(Orders.builder()
+                            .amount(1_000)
+                            .createdDate(LocalDate.of(2022,2,10))
+                            .itemName("item" + i)
+                            .build()))
                     .username("test username" + i)
                     .build());
         }
-        for (int i = 100; i < 200; i++) {
+        for (int i = 0; i < SIZE; i++) {
             users.add(User.builder()
-                    .totalAmount(200_000)
+                    .orders(Collections.singletonList(Orders.builder()
+                            .amount(200_000)
+                            .createdDate(LocalDate.of(2022,1,11))
+                            .itemName("item" + i)
+                            .build()))
                     .username("test username" + i)
                     .build());
         }
-        for (int i = 200; i < 300; i++) {
+        for (int i = 0; i < SIZE; i++) {
             users.add(User.builder()
-                    .totalAmount(300_000)
+                    .orders(Collections.singletonList(Orders.builder()
+                            .amount(300_000)
+                            .createdDate(LocalDate.of(2022,2,12))
+                            .itemName("item" + i)
+                            .build()))
                     .username("test username" + i)
                     .build());
         }
-        for (int i = 300; i < 400; i++) {
+        for (int i = 0; i < SIZE; i++) {
             users.add(User.builder()
-                    .totalAmount(500_000)
+                    .orders(Collections.singletonList(Orders.builder()
+                            .amount(500_000)
+                            .createdDate(LocalDate.of(2022,2,13))
+                            .itemName("item" + i)
+                            .build()))
                     .username("test username" + i)
                     .build());
         }
--- src/main/java/com/spring/batch/springbatchstudy/part4/User.java
+++ src/main/java/com/spring/batch/springbatchstudy/part4/User.java
@@ -1,11 +1,13 @@
 package com.spring.batch.springbatchstudy.part4;
 
+import com.spring.batch.springbatchstudy.part5.Orders;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 import javax.persistence.*;
 import java.time.LocalDate;
+import java.util.List;
 
 @Getter
 @Entity
@@ -21,18 +23,26 @@ public class User {
     @Enumerated(EnumType.STRING)
     private Level level = Level.NORMAL;
 
-    private int totalAmount;
+    @OneToMany(cascade = CascadeType.PERSIST) // entity 매핑 설정을 유저는 n개의 오더를 가질 수 있으니 원투 매니 / 유저가 저장되면서 오더를 같이 저장할 수 있도록 cascade persist 설정
+    @JoinColumn(name = "user_id")
+    private List<Orders> orders;
 
     private LocalDate updatedDate;
 
     @Builder
-    public User(String username, int totalAmount) {
+    public User(String username, List<Orders> orders) {
         this.username = username;
-        this.totalAmount = totalAmount;
+        this.orders = orders;
     }
 
     public boolean availableLevelUp() {
         return Level.availableLevelUp(this.getLevel(), this.getTotalAmount());
+    }
+
+    private int getTotalAmount() {
+        return this.orders.stream()
+                .mapToInt(Orders::getAmount)
+                .sum();
     }
 
     public Level levelUp() {
